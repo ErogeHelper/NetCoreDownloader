51c8fb38d0ec1fabd200191e08ca1a0ec43b13d5
k1mlka <k1mlka@outlook.com>
2021-12-14 04:55:07 +0800

    Update App.xaml.cs

--- NetCoreDownloader/App.xaml.cs
+++ NetCoreDownloader/App.xaml.cs
@@ -6,4 +6,21 @@ namespace NetCoreDownloader
     public partial class App : Application
     {
     }
+    
+    
+        private static Architecture GetPlatform()
+        {
+            if (RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
+            {
+                return Architecture.Arm64;
+            }
+            else if (Environment.Is64BitOperatingSystem)
+            {
+                return Architecture.X64;
+            }
+            else
+            {
+                return Architecture.X86;
+            }
+        }
 }
